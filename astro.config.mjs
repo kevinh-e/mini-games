9d5bc85ff09fef0f79afb1d4551310ce1af89f80
kevinh-e <account@kevinh.dev>
2024-12-09 02:31:53 +1100

    sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,18 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }),]
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon({
+      include: {
+        lucide: ["*"],
+      }
+    })]
 });
