cde8dc371751a6c263a3e894286c5846e883b3b1
kevinh-e <account@kevinh.dev>
2024-12-12 01:11:18 +1100

    completed prototype for all games

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -5,18 +5,26 @@ export const games = {
       {
         title: "2048",
         url: "/puzzle/2048",
+        description: "combine tiles to reach the 2048 tile.",
+        goal: ["score"]
       },
       {
         title: "slider",
         url: "/puzzle/slider",
+        description: "arrange scrambled tiles to recreate the original sequence.",
+        goal: ["time"]
       },
       {
         title: "tower of hanoi",
         url: "/puzzle/tower",
+        description: "solve the classic puzzle by moving discs between rods.",
+        goal: ["time", "moves"]
       },
       {
         title: "card match",
         url: "/puzzle/match",
+        description: "test your memory by matching pairs of cards.",
+        goal: ["time", "moves"]
       },
     ],
   },
@@ -26,18 +34,26 @@ export const games = {
       {
         title: "chess",
         url: "/strategy/chess",
+        description: "classic game of chess against ai or a friend.",
+        goal: ["time", "moves"]
       },
       {
         title: "minesweeper",
         url: "/strategy/minesweeper",
+        description: "uncover all safe squares without triggering hidden mines.",
+        goal: ["time", "moves"]
       },
       {
         title: "tic-tac-toe",
         url: "/strategy/tic-tac-toe",
+        description: "classic game of tic-tac-toe against ai or a friend.",
+        goal: ["time"]
       },
       {
         title: "snake",
         url: "/strategy/snake",
+        description: "fill the board while avoiding collisions.",
+        goal: ["time"]
       },
     ],
   },
@@ -47,10 +63,14 @@ export const games = {
       {
         title: "typing test",
         url: "/word/typing",
+        description: "test your typing speed and accuracy.",
+        goal: ["typing"]
       },
       {
         title: "wordle",
         url: "/word/wordle",
+        description: "based on the popular nyt daily word challenge",
+        goal: ["moves"]
       },
     ],
   },
